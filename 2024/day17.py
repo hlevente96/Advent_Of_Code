e882fefe4f9b352928ccde973ce1cc9e4350771c
leventeharsanyi <hlevente17@gmail.com>
2024-12-18 10:31:36 +0100

    Day 17/2 solution

--- 2024/day17.py
+++ 2024/day17.py
@@ -77,3 +77,40 @@ while pointer < len(program):
         pointer += 2
 
 print(outputs)
+
+def search_a(A):
+    out = []
+    while A !=0:
+        B = A % 8
+        B = B ^ 7
+        C = A >> B
+        B = B ^ 7
+        A = A >> 3
+        B = B ^ C
+        output = B % 8
+        out.append(output)
+    return out
+
+def finding_a(program):
+    options = {
+        0: [7],
+    }
+    rev_program = program[::-1]
+    for index in range(1,16):
+        option = options[index-1]
+        for op in option:
+            for i in range(8):
+                expected_output = rev_program[index]
+                output = search_a(op*8+i)[0]
+                if expected_output == output:
+                    if index not in options.keys():
+                        options[index] = []
+                    options[index].append(op*8+i)
+    return options
+
+program = [2,4,1,7,7,5,1,7,0,3,4,1,5,5,3,0]
+A_options = finding_a(program)
+solution = A_options[15][0]
+print(f"Solution: {solution}")
+resulting_sequence=search_a(solution)
+print(resulting_sequence)
